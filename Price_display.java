9387840ac6557e148847d68f2dd8f312aaed1b6b
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-11-02 11:46:32 +0000

    Created a Price_display class, which outputs a string with the name of the item in single quotes and the price in pounds sterling.

---
+++ Price_display.java
@@ -0,0 +1,7 @@
+class Price_Display {
+    public static void main(String[] args) {
+        String item = "Coffee Machine";
+        Double price = 15.99;
+        System.out.printf("The price for the item '%s', is £%.2f \n", item, price);
+    }
+}
